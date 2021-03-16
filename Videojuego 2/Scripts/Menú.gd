dc185954a1996ad98a4afb91d50cf57f3984e56d
TheErraticCookie <aram.marquez01@gmail.com>
2021-03-15 21:34:19 -0600

    Ready1 :3

--- Videojuego 2/Scripts/Menú.gd
+++ Videojuego 2/Scripts/Menú.gd
@@ -4,11 +4,15 @@ extends CanvasLayer
 
 func _on_BInstruciones_pressed():
 	#entrar al arbol de nodos
-	get_tree().change_scene("res://inicio/Controlinstrucciones.tscn")
+	get_tree().change_scene("res://Scenes/Instrucciones.tscn")
 
 func _on_BEmpezar_pressed():
-	get_tree().change_scene("res://EscenaJuego.tscn")
+	get_tree().change_scene("res://Scenes/EscenaJuego.tscn")
 
 
 func _on_BSalir_pressed():
 	get_tree().quit()
+
+
+func _on_VideoPlayer_finished():
+	get_tree().change_scene("res://Scenes/Instrucciones.tscn")
